e19cb53da99935f368edb01c222b6747aaa65308
LinusLoof <linus.loof96@gmail.com>
2021-05-14 13:24:44 +0000

    added templates/allauth

--- social_network/settings.py
+++ social_network/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'social',
     'landing',
 
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,7 +74,7 @@ ROOT_URLCONF = 'social_network.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
