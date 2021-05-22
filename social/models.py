7f557ff000b5aa3479639d07b853f1c8bf7ba180
LinusLoof <linus.loof96@gmail.com>
2021-05-22 05:46:46 +0000

    added the functionality for user to like and dislike posts

--- social/models.py
+++ social/models.py
@@ -10,6 +10,8 @@ class Post(models.Model):
     body = models.TextField()  # What the user tipes in on the post 
     created_on = models.DateTimeField(default=timezone.now)  # when the user post, its the the exact time
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # find the current user that creates the post
+    likes = models.ManyToManyField(User, blank=True, related_name='likes')
+    dislikes = models.ManyToManyField(User, blank=True, related_name='dislikes')
 
 
 # Comment model
--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, CommentEditView, ProfileView, ProfileEditView, AddFollower, RemoveFollower
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, CommentEditView, ProfileView, ProfileEditView, AddFollower, RemoveFollower, AddLike, Dislike
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -7,9 +7,12 @@ urlpatterns = [
     path('post/edit/<int:pk>/', PostEditView.as_view(), name='post-edit'),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/like', AddLike.as_view(), name='like'),
+    path('post/<int:pk>/dislike', Dislike.as_view(), name='dislike'),
     path('post/<int:post_pk>/comment/edit/<int:pk>/', CommentEditView.as_view(), name='comment-edit'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name='profile-edit'),
     path('profile/<int:pk>/followers/add', AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove', RemoveFollower.as_view(), name='remove-follower'),
+    
 ]
