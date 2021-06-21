7587f8003e4c2da25153f0877d37a5e5fdc110cb
LinusLoof <linus.loof96@gmail.com>
2021-06-21 12:02:03 +0000

    updated the link icon, updated the donation page

--- static/base.js
+++ static/base.js
@@ -13,3 +13,28 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+
+const searchInputDropdown = document.getElementById('search-input-dropdown');
+const dropdownOptions = document.querySelectorAll('.input-group-dropdown-item');
+
+searchInputDropdown.addEventListener('input', () => {
+  const filter = searchInputDropdown.value.toLowerCase();
+  showOptions();
+  const valueExist = !!filter.length;
+
+  if (valueExist) {
+    dropdownOptions.forEach((el) => {
+      const elText = el.textContent.trim().toLowerCase();
+      const isIncluded = elText.includes(filter);
+      if (!isIncluded) {
+        el.style.display = 'none';
+      }
+    });
+  }
+});
+
+const showOptions = () => {
+  dropdownOptions.forEach((el) => {
+    el.style.display = 'flex';
+  })
+}
